389370b10799c569a0b7ff7ed3204aa78536a1d5
dockerhopper <bransonancrawford@gmail.com>
2025-06-27 15:53:39 -0700

    Update site/security

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import htmlMinify from 'vite-plugin-html-minify';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [htmlMinify()]
+});
